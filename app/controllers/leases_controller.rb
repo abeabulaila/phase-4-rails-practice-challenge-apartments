fe25836c732262a2a64d67a1074794c6e5c730e1
Ibrahim Abulaila <abeabulaila@gmail.com>
2022-01-14 12:09:18 -0500

    yay

--- app/controllers/leases_controller.rb
+++ app/controllers/leases_controller.rb
@@ -2,7 +2,7 @@ class LeasesController < ApplicationController
   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_response
 
   def create
-    lease = Lease.create(lease_params)
+    lease = Lease.create!(lease_params)
     render json: lease, status: :created
   end
 
